05aedfde98e6f02b80d21fb66607bbc4987cb025
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-03-26 00:26:28 +0900

    add: init next.js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next"
+
+const Home: NextPage = (props) => {
+  return (
+    <div>
+      hogehoge
+      <div>pretteir</div>
+    </div>
+  )
+}
+
+export default Home
