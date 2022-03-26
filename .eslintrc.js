0ec94b8cd6e41a3f244e4c86a7d2ce15253e4111
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-03-26 20:34:37 +0900

    add: eslint prettier settings

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "next",
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  ignorePatterns: ["./.next"],
+}
