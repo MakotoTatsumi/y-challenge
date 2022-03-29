51b17d8a4ec3973c1dc333d7226e7b42262c5426
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-03-29 18:47:30 +0900

    update: API_keyをenvで管理

--- src/api/resas/apiClient/index.ts
+++ src/api/resas/apiClient/index.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 
 import api from "@/libs/apiClient/resas/$api"
 
-axios.defaults.headers.common["X-API-KEY"] = process.env.API_KEY || ""
+axios.defaults.headers.common["X-API-KEY"] = process.env.API_KEY || process.env.NEXT_PUBLIC_API_KEY || ""
 
 export const apiClient = api(axiosClient(axios))
