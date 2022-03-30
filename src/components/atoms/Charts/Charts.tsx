b2d507ad3a720a3bd3b8a7b581a8d013224c3d7b
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-03-31 05:31:59 +0900

    fix: Responsive対応

--- src/components/atoms/Charts/Charts.tsx
+++ src/components/atoms/Charts/Charts.tsx
@@ -1,15 +1,22 @@
 import React, { VFC } from "react"
+import { useMediaQuery } from "react-responsive"
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts"
 import styled from "styled-components"
 
+import { LEGEND_STYLE_PC, LEGEND_STYLE_SP } from "@components/atoms/Charts/Charts.const"
 import { ChartsType } from "@components/atoms/Charts/Charts.type"
 
 export const Charts: VFC<ChartsType> = (props) => {
   const { data, checked } = props
+  const isMobile = useMediaQuery({
+    query: "(max-width: 834px)",
+  })
+
+  const legendStyleProps = isMobile ? LEGEND_STYLE_SP : LEGEND_STYLE_PC
 
   return (
     <Root>
-      <ResponsiveContainer width={"90%"} height={400}>
+      <ResponsiveContainer width={"100%"} height={400}>
         <LineChart
           width={500}
           height={300}
@@ -25,10 +32,16 @@ export const Charts: VFC<ChartsType> = (props) => {
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Legend />
+          <Legend {...legendStyleProps} wrapperStyle={{ padding: "20px" }} />
           {checked.map((list) => {
             return (
-              <Line key={list.prefCode} type="monotone" dataKey={list.prefName} stroke="#8884d8" activeDot={{ r: 8 }} />
+              <Line
+                key={list.prefCode}
+                type="monotone"
+                dataKey={list.prefName}
+                stroke={list.colorCode}
+                activeDot={{ r: 8 }}
+              />
             )
           })}
         </LineChart>
