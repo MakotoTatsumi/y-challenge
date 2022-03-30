ed3536be2ff6c523fa9baf64d110a5e6d911fd9b
MakotoTatsumi <sakuchan.jp@gmail.com>
2022-03-31 01:27:36 +0900

    fix: url-prefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : ""
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "page.ts", "api.ts"],
   env: {
     API_KEY: process.env.API_KEY,
+    URL_PREFIX: process.env.URL_PREFIX,
   },
+  assetPrefix: urlPrefix,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
